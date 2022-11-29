4648ab2d058f423c76951d5d75c21cd398a4d16c
unknown <mr.aleksandrovv@inbox.ru>
2022-11-29 20:39:49 +0300

    33

--- app1/views.py
+++ app1/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, FileResponse
 from .models import Posts
-from django.core.files.storage import FileSystemStorage
-
+from django.core.files.storage import FileSystemStorage 
+import xml.etree.ElementTree as ET 
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 
 # Главная
 def index(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html', {'user': request.user})
 
 # Информация о сайте
 def inf(request):
+    print('user:', request.user)
     return render(request, 'inf.html')
 
 # Карточки гостей
@@ -46,7 +49,7 @@ def dog(request, id):
 
 # Редактировать пост
 def editpost(request, id):
-    editpost = Posts.objects.get(id=id)
+    dog = Posts.objects.get(id=id)
     return render(request, 'editpost.html', {'dog': dog})
 
 # Сохранить пост
@@ -69,5 +72,61 @@ def saveeditpost(request, id):
     dog.save()
     return HttpResponseRedirect('/guests')
 
+# Удалить пост
+def deletepost(request, id):
+    dog = Posts.objects.get(id=id)
+    dog.delete()
+    return HttpResponseRedirect('/guests')
 
 
+# Выгрузка
+def export(request):
+    posts = Posts.objects.all()
+    data = ET.Element('data')
+    for post in posts:
+        element = ET.SubElement(data, 'post')
+        element.set('title', post.name)
+
+    ET.ElementTree(data).write("posts.xml", encoding='UTF-8')
+    return HttpResponseRedirect('/guests')
+
+
+# Скачать XML
+def download(request):
+    f = open('posts.xml', 'rb')
+    return FileResponse(f,as_attachment=True)
+
+# Регистрация 
+def registration(request):
+    
+    if request.method =='POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        user = User.objects.create_user(name, email, password)
+        user.save()
+        return HttpResponseRedirect('/')
+    else:
+        return render(request, "registration.html")
+
+# Вход
+def login_user(request):
+    
+    if request.method =='POST':
+        name = request.POST.get('name')
+        password = request.POST.get('password')
+        user = authenticate(username=name, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect('/')
+        else:
+            return HttpResponseRedirect('/login')
+    else:
+        return render(request, "login.html")
+
+# Выход
+def logout_user(request):
+    logout(request)
+    return HttpResponseRedirect('/')
+
+    
--- myproject/urls.py
+++ myproject/urls.py
@@ -27,4 +27,11 @@ urlpatterns = [
     path('dog/<int:id>', views.dog),
     path('editpost/<int:id>', views.editpost),
     path('saveeditpost/<int:id>', views.saveeditpost),
+    path('deletepost/<int:id>', views.deletepost),
+    path('export', views.export),
+    path('download', views.download),
+    path('registration', views.registration),
+    path('login', views.login_user),
+    path('logout', views.logout_user),
+
 ]
